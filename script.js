715614dfb6370e4e0190c9b8ab359619767e8c34
anirudhsinghr <anirudhsinghr@gmail.com>
2017-01-01 23:36:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -128,4 +128,4 @@ document.getElementById("save").onclick = function () {
 	document.getElementById("red").style.backgroundColor = "red";
 	document.getElementById("green").style.backgroundColor = "green";
 	document.getElementById("blue").style.backgroundColor = "blue";
-})();
+})(); 
